c175ec22a77a65c3554abe58df7708279c07b9b1
robertolatini31 <robertolatini31@gmail.com>
2022-05-09 11:48:06 +0200

    added callapi function and tested

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data () {
+    return {
+      Api_Url: 'https://api.themoviedb.org/3/search/movie?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=it-IT&query=<<QUERY>>&page=1&include_adult=false',
+      Api_Key: '716ab35d3b7d9aab1757e0bac9e90c1c',
+      Query: '',
+      Movies: null,
+    }
+  },
+  methods: {
+    Api_Url_Generator(){
+      return 'https://api.themoviedb.org/3/search/movie?api_key=' + this.Api_Key + '&language=it-IT&query=' + this.Query + '&page=1&include_adult=false';
+    },
+    callApi() {
+      axios.get(this.Api_Url_Generator()).then((response) => {
+                console.log(response); 
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+  },
+  mounted() {
+        this.callApi();
+    },
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/scss/style.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: black;
+  color: white;
+  height: 100vh;
 }
 </style>
