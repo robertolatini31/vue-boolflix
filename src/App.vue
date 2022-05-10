95e0a16341a4d0cb25a64d9edf90c57175a9c57c
robertolatini31 <robertolatini31@gmail.com>
2022-05-10 19:16:58 +0200

    not work

--- src/App.vue
+++ src/App.vue
@@ -162,18 +162,16 @@ export default {
     IMG_Url_Generator(ApiImage){
       return 'https://image.tmdb.org/t/p/' + 'original' + ApiImage;
     },
-    CallCast(){
-       this.Movies.forEach(movie => {
-        axios.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=en-US').then((responseCast) => {
-                      movie.Actors = responseCast.data.cast.slice(0, 5);
-                  });
-      });
-    },
     CallMovies(){
         let MoviesUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.Api_Key + '&language=it-IT&query=' + this.Query + '&page=1&include_adult=false';
       return  axios.get(MoviesUrl).then((response) => {
                 //console.log(response);
                 this.Movies = response.data.results;
+                this.Movies.forEach(movie => {
+              axios.get('https://api.themoviedb.org/3/movie/' + movie.id + '/credits?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=en-US').then((responseCast) => {
+                      movie.Actors = responseCast.data.cast.slice(0, 5);
+                  });
+              });
             }).catch((error) => {
                 console.log(error);
             })
@@ -201,13 +199,7 @@ export default {
          Promise.all([
            this.CallMovies(),
            this.CallSeries(),
-         ]).then(
-           this.CallCast()
-         )
-
-          
-         
-      
+         ])
     }
   },
   mounted() {
