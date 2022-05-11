14dbf97e18efcb72e36799dc68c6c928eb131efc
robertolatini31 <robertolatini31@gmail.com>
2022-05-11 14:52:05 +0200

    added popular movies and series

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,74 @@
 
     <main class="p-3">
       <div class="container mt-4">
-      <div class="movies_if" v-if="Movies.length > 0">
-             <h2>Lista Film:</h2>
+
+
+      <div class="movies_if" v-if="!Movies.length > 0">
+             <h2>Film Popolari:</h2>
+      <div class="control_movies_true" v-if="FilteredPopularMovies.length > 0">
+        <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center">
+          <div class="col py-3 px-2 d-flex justify-content-center align-items-center" v-for="movie in FilteredPopularMovies" :key="movie.id">
+            <div class="cards_container">
+            <div class="card_container" :class="(movie.poster_path == null) ? 'bg_white' : ''">
+              <img :src="IMG_Url_Generator(movie.poster_path)" @error="$event.target.src='https://www.theoxygenstore.com/images/source/No-image.jpg'" :alt="movie.title">
+              <h5 class="safe_title m-0" v-if="movie.poster_path == null">{{movie.title}}</h5>
+            </div>
+            <!-- /.card_container -->
+            <div class="card_description p-2" :style="[(movie.backdrop_path != null ? {'background': 'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),' + 'url(' + IMG_Url_Generator(movie.backdrop_path) + ') center no-repeat'} : {'background': 'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5))'})]">
+              <div class="text_description">
+                <h5>Titolo: <span>{{movie.title}}</span></h5>
+              <h5>Titolo Originale: <span>{{movie.original_title}}</span></h5>
+              <h5>Lingua: <span>
+                            <LangFlag :iso="movie.original_language" />
+                          </span>
+              </h5>
+              <h5>Voto: <span>
+                          <font-awesome-icon icon="fa-solid fa-star" v-for="item in (StarVotes(movie.vote_average))"  :key="item" class="color_star" />
+                          <font-awesome-icon icon="fa-regular fa-star" v-for="item in (5 - StarVotes(movie.vote_average))"  :key="item + 5" class="color_star" />
+                        </span>
+              </h5>
+              <h5>Cast: <span v-for="(actor, index) in movie.Actors" :key="actor.id + 'ciao' + index">
+                          {{actor.name}} <br>
+                        </span>
+                       
+              </h5>
+              <h5>Generi: <span v-for="(GenreName, index) in FilterGenre(movie.genre_ids)" :key="GenreName + index">
+                            {{GenreName}} <br>
+                          </span>
+                          <span v-if="FilterGenre(movie.genre_ids).length == 0">Nessuna Informazione</span>
+              </h5>
+              <h5>Overview: 
+                <span v-if="movie.overview.length != 0">{{movie.overview}}</span>
+                <span v-else>Nessuna Descrizione Disponibile</span>
+              </h5>
+              </div>
+              <!-- /.text_description -->
+            </div>
+            <!-- /.card_description -->
+          </div>
+      <!-- /.cards_container -->
+          </div>
+          <!-- /.col -->
+
+          
+
+        </div>
+        <!-- /.row row-cols-5 -->
+      </div>
+      <!-- /.control_Movies -->
+
+      <div class="control_movies_false text-center text-white p-4" v-else>
+        <h2>Nessun Film Trovato</h2>
+        <p>cambiare filtri</p>
+      </div>
+      <!-- /.control_movies_false -->
+
+      </div>
+      <!-- /.movies_if ------------------------------------------------------------>
+
+
+      <div class="movies_if" v-else>
+             <h2>Film:</h2>
       <div class="control_movies_true" v-if="FilteredMovies.length > 0">
         <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center">
           <div class="col py-3 px-2 d-flex justify-content-center align-items-center" v-for="movie in FilteredMovies" :key="movie.id">
@@ -47,12 +113,12 @@
                         </span>
               </h5>
               <h5>Cast: <span v-for="(actor, index) in movie.Actors" :key="actor.id + 'ciao' + index">
-                          {{actor.name}}
+                          {{actor.name}} <br>
                         </span>
                        
               </h5>
               <h5>Generi: <span v-for="(GenreName, index) in FilterGenre(movie.genre_ids)" :key="GenreName + index">
-                            {{GenreName}}
+                            {{GenreName}} <br>
                           </span>
                           <span v-if="FilterGenre(movie.genre_ids).length == 0">Nessuna Informazione</span>
               </h5>
@@ -86,22 +152,69 @@
       <!-- /.movies_if -->
 
 
-      <!-- <ul v-for="movie in Movies" :key="movie.id">
-        <li>Titolo: {{movie.title}}</li>
-        <li>Titolo Originale: {{movie.original_title}}</li>
-        <!- <li>Lingua: {{movie.original_language}}</li> -->
-        <!-- <li>Lingua: <LangFlag :iso="movie.original_language" /></li>
-        <li>
-          Voto:
-          <font-awesome-icon icon="fa-solid fa-star" v-for="item in (StarVotes(movie.vote_average))"  :key="item" />
-          <font-awesome-icon icon="fa-regular fa-star" v-for="item in (5 - StarVotes(movie.vote_average))"  :key="item + 5" />
-        </li>
-        <li>IMG: {{IMG_Url_Generator(movie.poster_path)}}</li>
-      </ul> -->
+      
 
 
-      <div class="series_if" v-if="Series.length > 0">
-        <h2>Lista Serie:</h2>
+      <div class="series_if" v-if="!Series.length > 0">
+        <h2 class="pt_4">Serie Popolari:</h2>
+        <div class="control_series_true" v-if="FilteredPopularSeries.length > 0">
+        <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center">
+          <div class="col py-3 px-2 d-flex justify-content-center align-items-center" v-for="serie in FilteredPopularSeries" :key="serie.id">
+            <div class="cards_container">
+            <div class="card_container" :class="(serie.poster_path == null) ? 'bg_white' : ''">
+              <img :src="IMG_Url_Generator(serie.poster_path)" @error="$event.target.src='https://www.theoxygenstore.com/images/source/No-image.jpg'" :alt="serie.name">
+              <h5 v-if="serie.poster_path == null" class="safe_title m-0">{{serie.name}}</h5>
+            </div>
+            <!-- /.card_container -->
+            <div class="card_description p-2" :style="[(serie.backdrop_path != null ? {'background': 'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),' + 'url(' + IMG_Url_Generator(serie.backdrop_path) + ') center no-repeat'} : {'background': 'linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5))'})]">
+              <div class="text_description">
+                <h5>Titolo: <span>{{serie.name}}</span></h5>
+              <h5>Titolo Originale: <span>{{serie.original_name}}</span></h5>
+              <h5>Lingua: <span>
+                            <LangFlag :iso="serie.original_language" />
+                          </span>
+              </h5>
+              <h5>Voto: <span>
+                          <font-awesome-icon icon="fa-solid fa-star" v-for="item in (StarVotes(serie.vote_average))"  :key="item" class="color_star" />
+                          <font-awesome-icon icon="fa-regular fa-star" v-for="item in (5 - StarVotes(serie.vote_average))"  :key="item + 10" class="color_star" />
+                        </span>
+              </h5>
+              <h5>Cast: <span v-for="(actor, index) in serie.Actors" :key="actor.id + 'unique' + index">
+                          {{actor.name}} <br>
+                        </span>
+                        <!-- <span v-if="serie.Actors.length == 0">Nessuna Informazione</span> -->
+              </h5>
+              <h5>Generi: <span v-for="(GenreName, index) in FilterGenre(serie.genre_ids)" :key="index + GenreName">
+                            {{GenreName}} <br>
+                          </span>
+                          <span v-if="FilterGenre(serie.genre_ids).length == 0">Nessuna Informazione</span>
+              </h5>
+              <h5>Overview: 
+                <span v-if="serie.overview.length != 0">{{serie.overview}}</span>
+                <span v-else>Nessuna Descrizione Disponibile</span>
+              </h5>
+              </div>
+              <!-- /.text_description -->
+            </div>
+            <!-- /.card_description -->
+           </div>
+           <!-- /. cards_container  -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row row-cols-5 -->
+        </div>
+        <!-- /.control_series_true -->
+        <div class="control_series_false text-center text-white p-4" v-else>
+            <h2>Nessuna Serie Trovata</h2>
+            <p>cambiare filtri</p>
+        </div>
+        <!-- /.control_series_false -->
+      </div>
+      <!-- /.series_if ---------------------------------------------------->
+      
+      <div class="series_if" v-else>
+        <h2 class="pt_4">Serie:</h2>
         <div class="control_series_true" v-if="FilteredSeries.length > 0">
         <div class="row row-cols-xxl-5 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-1 justify-content-center">
           <div class="col py-3 px-2 d-flex justify-content-center align-items-center" v-for="serie in FilteredSeries" :key="serie.id">
@@ -125,12 +238,12 @@
                         </span>
               </h5>
               <h5>Cast: <span v-for="(actor, index) in serie.Actors" :key="actor.id + 'unique' + index">
-                          {{actor.name}} 
+                          {{actor.name}} <br>
                         </span>
                         <!-- <span v-if="serie.Actors.length == 0">Nessuna Informazione</span> -->
               </h5>
               <h5>Generi: <span v-for="(GenreName, index) in FilterGenre(serie.genre_ids)" :key="index + GenreName">
-                            {{GenreName}}
+                            {{GenreName}} <br>
                           </span>
                           <span v-if="FilterGenre(serie.genre_ids).length == 0">Nessuna Informazione</span>
               </h5>
@@ -158,18 +271,7 @@
       </div>
       <!-- /.series_if -->
      
-      <!-- <ul v-for="serie in Series" :key="serie.id">
-        <li>Titolo: {{serie.name}}</li>
-        <li>Titolo Originale: {{serie.original_name}}</li>
-        <!- <li>Lingua: {{serie.original_language}}</li> -->
-        <!-- <li>Lingua: <LangFlag :iso="serie.original_language" /></li>
-        <li>
-          Voto:
-          <font-awesome-icon icon="fa-solid fa-star" v-for="item in (StarVotes(serie.vote_average))"  :key="item" />
-          <font-awesome-icon icon="fa-regular fa-star" v-for="items in (5 - StarVotes(serie.vote_average))"  :key="items + 10" />
-        </li>
-        <li>IMG: {{IMG_Url_Generator(serie.poster_path)}}</li>
-      </ul> --> 
+      
 
       </div>
       <!-- /.container -->
@@ -195,6 +297,8 @@ export default {
       Movies: [],
       Series: [],
       Genres: [],
+      PopularMovies: [],
+      PopularSeries: [],
       GenreSelected: '',
     }
   },
@@ -209,7 +313,7 @@ export default {
       return 'https://image.tmdb.org/t/p/' + 'original' + ApiImage;
     },
     CallGenre() {
-        let GenreUrl = 'https://api.themoviedb.org/3/genre/list?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=it-IT';      
+        let GenreUrl = 'https://api.themoviedb.org/3/genre/list?api_key=' + this.Api_Key + '&language=it-IT';      
        axios.get(GenreUrl).then((response) => {
                 //console.log(response);
                 this.Genres = response.data.genres;
@@ -217,8 +321,41 @@ export default {
                 console.log(error);
             })
     },
+    CallPopularMovies(){
+      let PopularMoviesUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=' + this.Api_Key + '&language=it-IT&page=1';  
+      axios.get(PopularMoviesUrl).then((response) => {
+                //console.log(response);
+                this.PopularMovies = response.data.results;
+                this.PopularMovies.forEach(PopularMovie => {
+              axios.get('https://api.themoviedb.org/3/movie/' + PopularMovie.id + '/credits?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=en-US').then((responseCast) => {
+                      PopularMovie.Actors = responseCast.data.cast.slice(0, 5);
+                      this.$forceUpdate()
+                  });
+
+              });
+            }).catch((error) => {
+                console.log(error);
+            })
+    },
+    CallPopularSeries(){
+      let PopularSeriesUrl = 'https://api.themoviedb.org/3/tv/popular?api_key=' + this.Api_Key + '&language=it-IT&page=1';  
+      axios.get(PopularSeriesUrl).then((response) => {
+                //console.log(response);
+                this.PopularSeries = response.data.results;
+                this.PopularSeries.forEach(PopularSerie => {
+              axios.get('https://api.themoviedb.org/3/tv/' + PopularSerie.id + '/credits?api_key=716ab35d3b7d9aab1757e0bac9e90c1c&language=en-US').then((responseCast) => {
+                      PopularSerie.Actors = responseCast.data.cast.slice(0, 5);
+                      this.$forceUpdate()
+                  });
+
+              });
+            }).catch((error) => {
+                console.log(error);
+            })
+    },
     CallApi() {
-          let MoviesUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.Api_Key + '&language=it-IT&query=' + this.Query + '&page=1&include_adult=false';
+          if (this.Query.length > 0 && this.Query.replace(/\s/g, '').length > 0) {
+            let MoviesUrl = 'https://api.themoviedb.org/3/search/movie?api_key=' + this.Api_Key + '&language=it-IT&query=' + this.Query + '&page=1&include_adult=false';
           axios.get(MoviesUrl).then((response) => {
                 //console.log(response);
                 this.Movies = response.data.results;
@@ -248,6 +385,9 @@ export default {
                 console.log(error);
             })
          
+          } else {
+            this.Query = '';
+          }
     },
     FilterGenre(MovieGenres){
         let NamesGenres = [];
@@ -262,7 +402,9 @@ export default {
     },
   },
   mounted() {
-       this.CallGenre();
+      this.CallGenre();
+      this.CallPopularMovies();
+      this.CallPopularSeries();
   },
   computed: {
     FilteredMovies(){
@@ -278,7 +420,21 @@ export default {
            return Serie.genre_ids.includes(this.GenreSelected);
            })
         } else return this.Series;
-    }
+    },
+    FilteredPopularMovies(){
+        if (this.PopularMovies.length > 0 && this.GenreSelected != '') {
+          return  this.PopularMovies.filter(PopularMovie => {
+           return PopularMovie.genre_ids.includes(this.GenreSelected);
+           })
+        } else return this.PopularMovies;
+    },
+    FilteredPopularSeries(){
+        if (this.PopularSeries.length > 0 && this.GenreSelected != '') {
+          return  this.PopularSeries.filter(PopularSerie => {
+           return PopularSerie.genre_ids.includes(this.GenreSelected);
+           })
+        } else return this.PopularSeries;
+    },
   },
 }
 </script>
@@ -286,6 +442,9 @@ export default {
 <style lang="scss">
 @import '@/assets/scss/style.scss';
 #app {
+  .pt_4 {
+    padding-top: 4rem!important;
+  }
   header {
     box-shadow: 0px -16px 20px 0px white;
     position: fixed;
@@ -358,6 +517,10 @@ export default {
       border-radius: 4px;
       .text_description {
         filter: drop-shadow(2px 4px 6px black);
+        span {
+          font-size: 1rem;
+          font-weight: 400;
+        }
       }
     }
     .card_container:hover + .card_description, .card_description:hover {
